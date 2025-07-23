74d1275039e91e35649b28a574cfdb0586a01769
Ali-gunner <ali.ranjbar.lut2024@gmail.com>
2025-07-24 00:11:59 +0300

    Added environment variable

--- app.js
+++ app.js
@@ -6,8 +6,17 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 // Middlewares
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 🖐');
+  next();
+});
 
 // app.get('/api/v1/tours', getAllTours);
 // app.get('/api/v1/tours/:id', getTour);
